84bc4a45b319707bb7108e02f45df115569fefe4
Jono Sellier <jonosellier@gmail.com>
2020-09-28 16:10:36 -0400

    Added error handling, etc

--- index.js
+++ index.js
@@ -2,17 +2,43 @@
 
 const fs = require('fs');
 const fetch = require('node-fetch');
+const path = require('path');
+const { exit } = require('process');
 
 const args = process.argv.slice(2);
 const jsQueryString = args.slice(1).join(' ');
-const path = args[0];
+const fpath = args[0];
 let root;
 
 async function main() {
-    if (path.match(/https?:\/\//)) { //is URL
-        const res = await fetch(path);
-        root = await res.json();
-    } else root = JSON.parse(fs.readFileSync(args[0]).toString());
+    if (!fpath) {
+        console.log('USAGE:\n$ jr URI/path jsQueryString');
+        console.log('URI/path:');
+        console.log('Please specify protocol (http/https) if it is a URI, otherwise relative or absolute path works for local.');
+        console.log('jsQueryString:');
+        console.log('The data you want returned in root.prop or root[\'prop\'] syntax. The literal JSON data is returned by root. You can also use any JS function as well as included shapeOf(obj: Object) function to get the shape of an object.\nShorthand for shapeOf(root) is shape');
+        exit(1);
+    }
+    if (!jsQueryString) {
+        console.log('No arguments provided.');
+        console.log('Use root to access the entire JSON response and access items with [] or . syntax as needed');
+        console.log('Use shapeOf(obj: Object) to get TypeScript-style shape for any property.\nShorthand for shapeOf(root) is shape');
+        exit(1);
+    }
+    if (fpath.match(/https?:\/\//)) { //is URL
+        const res = await fetch(fpath)
+            .catch(e => {
+                console.log(`ERROR: fetching from URL (${fpath}) failed.`);
+                exit(1);
+            });
+        if (res) root = await res.json();
+    } else {
+        if (fs.existsSync(path)) {
+            root = JSON.parse(fs.readFileSync(args[0]).toString());
+        } else {
+
+        }
+    }
 
 
     let myObj;
@@ -29,18 +55,21 @@ async function main() {
     }
 }
 
-function shapeOf(obj, depth = 0) {
+function shapeOf(obj, depth = 1) {
     const indents = depth > 0 ? '  ' : ''; //add an indent in every nested object except the first
-    if(!obj) return (JSON.stringify(null, false, 2).replace(/\"/g, '')).replace(/\\n/g, '\n' + indents);
     if (typeof obj !== 'object') { //handles string, number, boolean, null etc.
         return typeof obj;
     } else { //handles object or array
         let outObj = {};
         if (Array.isArray(obj)) { //handles array
-            outObj = parseArray(obj, depth);
+            if (!obj[0]) return '[]';
+            for (const prop in obj[0]) {
+                outObj[prop] = shapeOf(obj[0][prop], 1);
+            }
+            return (JSON.stringify(outObj, false, 2).replace(/\"/g, '')).replace(/\\n/g, ' ' + depth + '\n' + indents) + '[]';
         } else { //handles object
             for (const prop in obj) {
-                outObj[prop] = shapeOf(obj[prop], depth + 1);
+                outObj[prop] = shapeOf(obj[prop], 1);
             }
         }
         return (JSON.stringify(outObj, false, 2).replace(/\"/g, '')).replace(/\\n/g, '\n' + indents);
@@ -49,7 +78,7 @@ function shapeOf(obj, depth = 0) {
 
 function parseArray(arr, depth) {
     if (arr.length == 0) return '[]'; //empty array
-    else return shapeOf(arr[0], depth) + '[]'; //get the type of the first element
+    else return shapeOf(arr[0], 0) + '[]'; //get the type of the first element
 }
 
 main();
